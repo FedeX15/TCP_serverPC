05f7604f35abd28a5eaa7bec170a70c1f9adc0f4
Federico Matteoni <fedex15@live.it>
2015-06-12 18:34:45 +0200

    Implementazione iniziale pong

--- src/com/fedex/tcpserver/TCPSocket_thread.java
+++ src/com/fedex/tcpserver/TCPSocket_thread.java
@@ -36,12 +36,13 @@ public class TCPSocket_thread extends Thread
                 } else if (input.equals("GetServerInfo")) {
                     gui.setOutputStatus("[" + this.getName().split("/")[1] + "] " + "<Request: ServerInfo>");
                     server.send(InetAddress.getByName(this.getName().split("/")[1]), "Server " + (server.avviato ? "online" : "offline") + "|" + server.clientConnessi + " client");
-                } else if (input.equals("StreamCamera")) {
+                } else if (input.equals("StartUDPStream")) {
                     DatagramSocket streamsocket = new DatagramSocket(8890);
-                    byte[] recvBuf = ("ready").getBytes();
-                    DatagramPacket recvPacket = new DatagramPacket(recvBuf, recvBuf.length, InetAddress.getByName(this.getName().split("/")[1]), 8890);
+                    byte[] recvBuf = new byte[1500];
+                    DatagramPacket recvPacket = new DatagramPacket(recvBuf, recvBuf.length);
                     StreamGUI streamgui = new StreamGUI();
                     streamgui.setVisible(true);
+                    streamgui.setBounds(this.gui.getX() + this.gui.getWidth()/2, this.gui.getY() + this.gui.getHeight()/2, streamgui.getWidth(), streamgui.getHeight());
                     String txt;
                     do {
                         streamsocket.receive(recvPacket);
@@ -49,6 +50,29 @@ public class TCPSocket_thread extends Thread
                         streamgui.setText(txt);
                     } while (!txt.equals("Close"));
                     streamgui.setVisible(false);
+                    streamsocket.close();
+                } else if (input.startsWith("Play")) {
+                    String str = input.split("&")[1];
+                    int w = Integer.parseInt(str.split("-")[0]);
+                    int h = Integer.parseInt(str.split("-")[1]);
+                    DatagramSocket streamsocket = new DatagramSocket(8890);
+                    byte[] recvBuf = new byte[1500];
+                    DatagramPacket recvPacket = new DatagramPacket(recvBuf, recvBuf.length);
+                    PlayGUI playgui = new PlayGUI(w/2, h/2);
+                    playgui.setVisible(true);
+                    playgui.setBounds(this.gui.getX() + this.gui.getWidth()/2, this.gui.getY() + this.gui.getHeight()/2, playgui.getWidth(), playgui.getHeight());
+                    String txt;
+                    int x;
+                    do {
+                        streamsocket.receive(recvPacket);
+                        txt = new String(recvBuf, 0, recvPacket.getLength());
+                        try {
+                            x = Integer.parseInt(txt);
+                            playgui.setOpponentPosition(x/2);
+                        } catch (NumberFormatException ex) {
+                        }
+                    } while (!txt.equals("Close"));
+                    playgui.setVisible(false);
                     streamsocket.close();
                 } else {
                     gui.setOutputStatus("[" + this.getName().split("/")[1] + "] " + input);
